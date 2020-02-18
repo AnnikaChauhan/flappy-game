a99918884e85ac9151848b67f7ef0f147787f157
Annika Chauhan <annikachauhan@Annikas-MacBook-Pro.local>
2020-02-18 08:42:56 +0000

    slight css animation changes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const gameStartUp = () => {
-    
-
+    const dragon = $('#dragon');
+    document.onkeydown = () => {
+        if(event.which === 32) {
+            //connection tests
+            console.log('space');
+            dragon.css({'color':'red'});
+        }
+    }
 }
 
 $(gameStartUp);
