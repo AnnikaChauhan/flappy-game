c9b1d75d005021ff6da02bf8b5664da2a1b1677f
Annika Chauhan <annikachauhan@annikas-mbp.home>
2020-02-18 20:21:14 +0000

    gravity working

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const gameStartUp = () => {
-    const dragon = $('#dragon');
+    const ball = document.getElementById("ball");
+    let ballTop = 200;
+
+    const fallingBall = () => {
+        const myInterval = setInterval(() => {
+            if (ballTop < 480) {
+                ballTop +=1 ;
+                ball.style.top = ballTop;
+                //console.log(ballTop);
+            } 
+        }, 100);
+    }
+
+    const addObstacle = () => {
+        let obstacleRightStart = 0;
+        const obstacle = $('section').append('<div class="obstacle"></div>');
+
+        obstacleRightStart += 2;
+    }
+
+    addObstacle();
+
+    //begin the game
+    document.onkeypress = () => {
+        if(event.which === 13){
+            console.log('enter');
+            fallingBall();
+            //setTimeout(addObstacle,1000);
+            //click enter/spacebar to begin the setInterval
+            //wait 10 seconds and then begin the obstacles
+        }   
+    }
+
+    // initiate game play with space bars - have collision logic here
     document.onkeydown = () => {
         if(event.which === 32) {
-            //connection tests
-            console.log('space');
-            dragon.css({'color':'red'});
+            ballTop -= 40;
+            ball.style.top = ballTop;
         }
     }
 }
